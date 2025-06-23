406ff29f1ebb348462c275e533400137e518fd16
airflow-workstation <ec2-user@ip-172-31-12-148.ap-northeast-1.compute.internal>
2025-06-23 09:31:32 +0000

    update new status

--- unittests/test_dag_validation.py
+++ unittests/test_dag_validation.py
@@ -4,7 +4,7 @@ from airflow.models import DagBag
 class TestDagValidation:
 
     LOAD_SECOND_THRESHOLD = 2
-    REQUIRED_EMAIL = "support1@airflow.com"
+    REQUIRED_EMAIL = "support@airflow.com"
     EXPECTED_NUMBER_OF_DAGS = 1
 
     def test_import_dags(self, dagbag):
